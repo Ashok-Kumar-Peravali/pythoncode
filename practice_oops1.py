8066dd46aa6a7963ecf646cba6f35e786382d31d
Ashok-Kumar-Peravali <ashok.peravali@gmail.com>
2025-10-28 19:37:51 +0530

    First commit 19:27 28-Oct-2025

---
+++ practice_oops1.py
@@ -0,0 +1,17 @@
+class Computer:
+    def __init__(self, cpu, ram, disk):
+        self.cpu = cpu
+        self.ram = ram
+        self.disk = disk
+
+
+    def config(self):
+        print("Computer config is:",self.cpu, "CPU", self.ram, "RAM", self.disk, "HDD")
+
+
+com1 = Computer('i5','16GB', '500GB')
+com2 = Computer('AMD', '32GB', '1TB')
+
+
+com1.config()
+com2.config()
