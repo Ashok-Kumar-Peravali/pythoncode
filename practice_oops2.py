d3e4c7d6254d34058cf359d8f8c515e5fc8855b9
Ashok-Kumar-Peravali <ashok.peravali@gmail.com>
2025-10-28 20:10:10 +0530

    20:10 28-Oct-2025

--- practice_oops2.py
+++ practice_oops2.py
@@ -1,9 +1,27 @@
+from operator import truediv
+
+
 class Computer:
-    pass
+    def __init__(self):
+        self.name = "AShok"
+        self.age = 45
+    def update(self):
+        self.age += 2
+    def compare(self, other):
+        if self.age == other.age:
+            return True
+        else:
+            return False
 
 
 c1=Computer()
 c2=Computer()
+c2.name = "Priya"
+print(c1.name,c1.age)
+#c2.update()
+print(c2.name,c2.age)
 
-print(id(c1))
-print(id(c2))
+if c1.compare(c2):
+    print("They are the same")
+else:
+    print("They are different")
